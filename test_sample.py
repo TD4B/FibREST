c4c055062e58f5a5aa00ec4493d1c1c4b31c1254
Thomas West <edwin.twest@gmail.com>
2018-05-05 16:10:32 -0700

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,10 +1,17 @@
 import fibl as fib
 
-def test_answer():
+def test_answers():
+  # Test Cases for the Function.
   a = 5
   b = 21
   c = 75025
+  d = float("inf")
+  
+  # Evaluating the results.
   at = round(fib.Fibonacci(5))
-  ab = round(fib.Fibonacci(8))
-  ac = round(fib.Fibonacci(25))
-  assert (a, b, c) == (at, ab, ac)
+  bt = round(fib.Fibonacci(8))
+  ct = round(fib.Fibonacci(25))
+  dt = fib.Fibonacci(2000)
+  
+  # Checking the known answers against our calculation.
+  assert (a, b, c, d) == (at, bt, ct, dt)
