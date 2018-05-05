3c17bc17c764d22206155f61e7ef1211f11799b2
Thomas West <edwin.twest@gmail.com>
2018-05-05 15:57:52 -0700

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,10 @@
 import fibl as fib
 
 def test_answer():
-  assert round(fib.Fibonacci(5)) == 6
+  a = 5
+  b = 21
+  c = 75025
+  at = round(fib.Fibonacci(5))
+  ab = round(fib.Fibonacci(8))
+  ac = round(fib.Fibonacci(5))
+  assert (a,b,c) == (at,ab,ac)
