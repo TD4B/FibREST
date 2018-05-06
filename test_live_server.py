c737376979e012ff8bdac59e94d558a3997b4975
Thomas West <edwin.twest@gmail.com>
2018-05-06 11:39:48 -0700

    Update test_live_server.py

--- test_live_server.py
+++ test_live_server.py
@@ -1,23 +1,21 @@
 import pytest
+
 try:
-    from urllib2 import urlopen
+	from urllib2 import urlopen
 except ImportError:
-    from urllib.request import urlopen
+	from urllib.request import urlopen
 
 from flask import url_for
 
+def test_server_is_alive(self, live_server):
+	assert live_server._process
+	assert live_server._process.is_alive()
 
-class TestLiveServer:
+def test_server_url(self, live_server):
+	assert live_server.url() == 'http://localhost:%d' % live_server.port
+	assert live_server.url('/ping') == 'http://localhost:%d/ping' % live_server.port
 
-    def test_server_is_alive(self, live_server):
-        assert live_server._process
-        assert live_server._process.is_alive()
-
-    def test_server_url(self, live_server):
-        assert live_server.url() == 'http://localhost:%d' % live_server.port
-        assert live_server.url('/ping') == 'http://localhost:%d/ping' % live_server.port
-
-     def test_server_listening(self, live_server):
-        res = urlopen(live_server.url('/ping'))
-        assert res.code == 200
-        assert b'pong' in res.read()
+def test_server_listening(self, live_server):
+	res = urlopen(live_server.url('/ping'))
+	assert res.code == 200
+	assert b'pong' in res.read()
