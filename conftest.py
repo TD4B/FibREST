17f68c2239e79826446274cb1d18b909bd42a75f
Thomas West <edwin.twest@gmail.com>
2018-05-06 11:43:32 -0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,24 +1,7 @@
-import pytest
-
-from flask import Flask, jsonify
-
-
-pytest_plugins = 'pytester'
-
+from myapp import create_app
 
 @pytest.fixture
 def app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = '42'
-
-    @app.route('/')
-    def index():
-        return app.response_class('OK')
-
-    @app.route('/ping')
-    def ping():
-        return jsonify(ping='pong')
-
+    app = create_app()
     return app
-
 
